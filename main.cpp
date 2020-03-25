3d3672ed4ea35b48fedfe9e0b1d76f5af0903d6c
Derrick Lau <derrick.evltfmr@gmail.com>
2020-03-25 08:54:14 -0500

    [Ver9] Count each player pieces and keep track of the game phrases

--- main.cpp
+++ main.cpp
@@ -28,7 +28,14 @@ AIMode ai_mode = AI_MODE_NOT_SET;
 ColorOrderMode color_order_mode = COLOR_ORDER_NOT_SET;
 PlayerTurn player_turn = TURN_NOT_DEFINED;
 PlayerColor player_color = COLOR_NOT_DEFINED;
-GamePhrase game_phrase = OPENING;
+
+GamePhrase your_game_phrase = OPENING;
+GamePhrase opponent_game_phrase = OPENING;
+
+int num_your_pieces;
+int num_opponent_pieces;
+int num_your_removed_pieces;
+int num_opponent_removed_pieces;
 
 int main(){
     
@@ -66,7 +73,7 @@ void menu() {
         colorOrderModeMenu();
     }
 
-    // turn and color
+    // set turn and color
     if (color_order_mode == YOU_WHITE_FIRST){
         player_turn = YOUR_TURN;
         player_color = YOU_WHITE_COLOR;
@@ -84,15 +91,24 @@ void menu() {
         player_color = YOU_WHITE_COLOR;
     }
 
+    // initialize/reset num of pieces, game phrase
+    num_your_pieces = 9;
+    num_opponent_pieces = 9;
+    num_your_removed_pieces = 0;
+    num_opponent_removed_pieces = 0;
+
+    your_game_phrase = OPENING;
+    opponent_game_phrase = OPENING;
 }
 
 void gameModeMenu() {
     string str_choice;
     int choice = -1;
+    bool inputInvalid = false;
 
-    system("clear");
     displayMenu:
-    
+    system("clear");
+
     cout << "==================================================================== " << endl;
     cout << "|| ############   NINE MEN'S MORRIS GAME VARIANT-B   ############ || " << endl;
     cout << "==================================================================== " << endl;
@@ -102,6 +118,12 @@ void gameModeMenu() {
     cout << "[2]: AI opponent Mode" << endl;
 
     cout << endl << "Choice: " << endl;
+
+    if (inputInvalid){
+        // error message
+        cout << "\n\n The input is not valid\n\n" << endl;
+    }
+
     cin >> str_choice;
 
     choice = stoi(str_choice); //stoi in <string>
@@ -120,9 +142,8 @@ void gameModeMenu() {
         break;
 
     default:
-        system("clear");
-        // error message
-        cout << "\n\n The input is not valid \n\n" << endl;
+        // set input invalid
+        inputInvalid = true;
 
         // continue to display menu
         goto displayMenu;
@@ -134,10 +155,11 @@ void gameModeMenu() {
 void aiModeMenu() {
     string str_choice;
     int choice = -1;
+    bool inputInvalid = false;
 
-    system("clear");
     displayMenu:
-    
+    system("clear");
+
     cout << "==================================================================== " << endl;
     cout << "|| ############   NINE MEN'S MORRIS GAME VARIANT-B   ############ || " << endl;
     cout << "==================================================================== " << endl;
@@ -146,6 +168,12 @@ void aiModeMenu() {
     cout << "[1] AI AUTO: makes the move automatically" << endl;
 
     cout << endl << "Choice: " << endl;
+
+    if (inputInvalid){
+        // error message
+        cout << "\n\n The input is not valid\n\n" << endl;
+    }
+
     cin >> str_choice;
 
     choice = stoi(str_choice); //stoi in <string>
@@ -160,9 +188,8 @@ void aiModeMenu() {
         break;
 
     default:
-        system("clear");
-        // error message
-        cout << "\n\n The input is not valid\n\n" << endl;
+        // set input invalid
+        inputInvalid = true;
 
         // continue to display menu
         goto displayMenu;
@@ -174,10 +201,11 @@ void aiModeMenu() {
 void colorOrderModeMenu(){
     string str_choice;
     int choice = -1;
+    bool inputInvalid = false;
 
-
-    system("clear");
+    
     displayMenu:
+    system("clear");
     
     cout << "==================================================================== " << endl;
     cout << "|| ############   NINE MEN'S MORRIS GAME VARIANT-B   ############ || " << endl;
@@ -189,6 +217,12 @@ void colorOrderModeMenu(){
     cout << "[3] Opponent choose BLACK, and Opponent move FIRST" << endl;
 
     cout << endl << "Choice: " << endl;
+
+    if (inputInvalid){
+        // error message
+        cout << "\n\n The input is not valid\n\n" << endl;
+    }
+
     cin >> str_choice;
 
     choice = stoi(str_choice); //stoi in <string>
@@ -211,9 +245,8 @@ void colorOrderModeMenu(){
         break;
 
     default:
-        system("clear");
-        // error message
-        cout << "\n\n The input is not valid\n\n" << endl;
+        // set input invalid
+        inputInvalid = true;
 
         // continue to display menu
         goto displayMenu;
@@ -240,6 +273,26 @@ void run(){
         player_turn = OPPONENT_TURN;
     else if (player_turn == OPPONENT_TURN)
         player_turn = YOUR_TURN;
+
+    // switch game phrase
+    int num_your_remaining_pieces = 9 - num_your_removed_pieces; 
+    int num_opponent_remaining_pieces = 9 - num_opponent_removed_pieces; 
+    
+    if (your_game_phrase == OPENING){
+        if (num_your_pieces == 0) your_game_phrase = MIDGAME;
+        if (num_your_remaining_pieces == 3) your_game_phrase = ENDGAME;
+    }
+    if (your_game_phrase == MIDGAME){
+        if (num_your_remaining_pieces == 3) your_game_phrase = ENDGAME;
+    }
+
+    if (opponent_game_phrase == OPENING){
+        if (num_opponent_pieces == 0) opponent_game_phrase = MIDGAME;
+        if (num_opponent_remaining_pieces == 3) opponent_game_phrase = ENDGAME;
+    }
+    if (opponent_game_phrase == MIDGAME){
+        if (num_opponent_remaining_pieces == 3) opponent_game_phrase = ENDGAME;
+    }
 
     system("clear");
     
@@ -283,13 +336,19 @@ struct inputData inputValue(string symbol[21],int count){
     int i;
     struct inputData info;
 
+    bool inputInvalid = false;
 
-    if (game_phrase==OPENING){
-        // input again point
-        inputAgain:
 
-        // count to keep track of BW's turn
-        if(player_turn == YOUR_TURN){
+    // input again point
+    inputAgain:
+
+    // count to keep track of BW's turn
+
+    // [[YOUR TURN]]
+    if (player_turn == YOUR_TURN){
+
+        // OPENING
+        if (your_game_phrase == OPENING){
             if (player_color == YOU_WHITE_COLOR){
                 printf("\n\n[[### YOUR TURN ###]]");
                 printf("\n\nEnter Your Choice [White] (W):");
@@ -300,7 +359,12 @@ struct inputData inputValue(string symbol[21],int count){
             }
             else printf("ERROR: player_color is not WHITE nor BLACK");
         }
-        else if (player_turn == OPPONENT_TURN){
+    }
+    // [[OPPONENT'S TURN]]
+    else if (player_turn == OPPONENT_TURN){
+
+        // OPENING
+        if (your_game_phrase == OPENING){
             if (player_color == YOU_BLACK_COLOR){
                 printf("\n\n[[### OPPONENT'S TURN ###]]");
                 printf("\n\nEnter Opponent Choice [White] (W):");
@@ -309,35 +373,43 @@ struct inputData inputValue(string symbol[21],int count){
                 printf("\n\n[[### OPPONENT'S TURN ###]]");
                 printf("\n\nEnter Opponent Choice [Black] (B):");
             }
-        }
-        else printf("ERROR: player_turn is not YOUR TURN nor OPPONENT TURN");
-
-        // read the input
-        cin >> value;
-        
-        for(i=0;i<21;i++){
-
-            if(value == symbol[i]){   // convert to string and compare
-                info.i = i;
-                if(player_turn == YOUR_TURN && player_color == YOU_WHITE_COLOR)
-                    info.ch = " W";
-                else if(player_turn == YOUR_TURN && player_color == YOU_BLACK_COLOR)
-                    info.ch = " B";
-                else if(player_turn == OPPONENT_TURN && player_color == YOU_BLACK_COLOR)
-                    info.ch = " W";
-                else if(player_turn == OPPONENT_TURN && player_color == YOU_WHITE_COLOR)
-                    info.ch = " B";
-                break;
-            }else{
-                info.i = -1;
-                info.ch = ' ';
-            }
-        }
-        if(info.i == -1){
-            printf("\nInput is not Valid");
-            goto inputAgain;
+            else printf("ERROR: player_color is not WHITE nor BLACK");
         }
     }
+    else printf("ERROR: player_turn is not YOUR TURN nor OPPONENT TURN");
+
+    if (inputInvalid){
+        // error message
+        cout << "\n\n The input is not valid\n\n" << endl;
+    }
+
+    // read the input
+    cin >> value;
+    
+    for(i=0;i<21;i++){
+
+        if(value == symbol[i]){   // convert to string and compare
+            info.i = i;
+            if(player_turn == YOUR_TURN && player_color == YOU_WHITE_COLOR)
+                info.ch = " W";
+            else if(player_turn == YOUR_TURN && player_color == YOU_BLACK_COLOR)
+                info.ch = " B";
+            else if(player_turn == OPPONENT_TURN && player_color == YOU_BLACK_COLOR)
+                info.ch = " W";
+            else if(player_turn == OPPONENT_TURN && player_color == YOU_WHITE_COLOR)
+                info.ch = " B";
+            break;
+        }else{
+            info.i = -1;
+            info.ch = ' ';
+        }
+    }
+    if(info.i == -1){
+        // set input invalid
+        inputInvalid = true;
+        goto inputAgain;
+    }
+    
 
     return info;
 }
@@ -351,24 +423,40 @@ void displayGameBoard(string symbol[21]){
 
 	printf("=====================================================================================\n");
 	printf("\t\t         Nine Men's Morris Game Variant-B");
-    printf("\n=====================================================================================");
+    printf("\n=====================================================================================\n");
+    // remaining pieces
+    int num_your_remaining_pieces = 9 - num_your_removed_pieces; 
+    int num_opponent_remaining_pieces = 9 - num_opponent_removed_pieces; 
+
+    // str_gamephrase
+    string your_str_gamephrase;
+    string opponent_str_gamephrase;
+
+    if (your_game_phrase == OPENING) your_str_gamephrase = "Opening";
+    else if (your_game_phrase == MIDGAME) your_str_gamephrase = "Midgame";
+    else if (your_game_phrase == ENDGAME) your_str_gamephrase = "Endgame";
+
+    if (opponent_game_phrase == OPENING) opponent_str_gamephrase = "Opening";
+    else if (opponent_game_phrase == MIDGAME) opponent_str_gamephrase = "Midgame";
+    else if (opponent_game_phrase == ENDGAME) opponent_str_gamephrase = "Endgame";
+
     if (color_order_mode == YOU_WHITE_FIRST) {
-        printf("\n[Players 1 (YOU     ) symbol: [White](W)]");
-        printf("\n[Players 2 (OPPONENT) symbol: [Black](B)]");
+        printf("\nPlayer1(YOU     )[White](W) || Free pieces: %d || Total remaining pieces: %d || Game phrase: %s", num_your_pieces, num_your_remaining_pieces, your_str_gamephrase.c_str());
+        printf("\nPlayer2(OPPONENT)[Black](B) || Free pieces: %d || Total remaining pieces: %d || Game phrase: %s", num_opponent_pieces, num_opponent_remaining_pieces, opponent_str_gamephrase.c_str());
     }
     if (color_order_mode == YOU_BLACK_FIRST) {
-        printf("\n[Players 1 (YOU     ) symbol: [Black](B)]");
-        printf("\n[Players 2 (OPPONENT) symbol: [White](W)]");
+        printf("\nPlayer1(YOU     )[Black](B) || Free pieces: %d || Total remaining pieces: %d || Game phrase: %s", num_your_pieces, num_your_remaining_pieces, your_str_gamephrase.c_str());
+        printf("\nPlayer2(OPPONENT)[White](W) || Free pieces: %d || Total remaining pieces: %d || Game phrase: %s", num_opponent_pieces, num_opponent_remaining_pieces, opponent_str_gamephrase.c_str());
     }
     if (color_order_mode == OPPONENT_WHITE_FIRST) {
-        printf("\n[Players 1 (OPPONENT) symbol: [White](W)]");
-        printf("\n[Players 2 (YOU     ) symbol: [Black](B)]");
+        printf("\nPlayer1(OPPONENT)[White](W) || Free pieces: %d || Total remaining pieces: %d || Game phrase: %s", num_opponent_pieces, num_opponent_remaining_pieces, opponent_str_gamephrase.c_str());
+        printf("\nPlayer2(YOU     )[Black](B) || Free pieces: %d || Total remaining pieces: %d || Game phrase: %s", num_your_pieces, num_your_remaining_pieces, your_str_gamephrase.c_str());
     }
     if (color_order_mode == OPPONENT_BLACK_FIRST) {
-        printf("\n[Players 1 (OPPONENT) symbol: [Black](B)]");
-        printf("\n[Players 2 (YOU     ) symbol: [White](W)]");
+        printf("\nPlayer1(OPPONENT)[Black](B) || Free pieces: %d || Total remaining pieces: %d || Game phrase: %s", num_opponent_pieces, num_opponent_remaining_pieces, opponent_str_gamephrase.c_str());
+        printf("\nPlayer2(YOU     )[White](W) || Free pieces: %d || Total remaining pieces: %d || Game phrase: %s", num_your_pieces, num_your_remaining_pieces, your_str_gamephrase.c_str());
     }
-	printf("\n\t\t\t");
+	printf("\n\n\t\t\t");
 	printf("\n\t      \t  6     %s-------------%s-------------%s",symbol[18].c_str(),symbol[19].c_str(),symbol[20].c_str());
 	printf("\n\t      \t         |              |              |");
 	printf("\n\t      \t  5      |   %s--------%s--------%s    |",symbol[15].c_str(),symbol[16].c_str(),symbol[17].c_str());
